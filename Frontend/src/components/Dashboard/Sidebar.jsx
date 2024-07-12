dda5dcb805f33e601ccf803ab92bd92f4932a0c0
ajay90989 <ajay.office.patidar>
2024-07-12 16:57:36 +0530

    Done

--- Frontend/src/components/Dashboard/Sidebar.jsx
+++ Frontend/src/components/Dashboard/Sidebar.jsx
@@ -4,24 +4,46 @@ import { superadmin_header, admin_header } from "./Sidebar_path";
 
 const Sidebar = () => {
   const roles = JSON.parse(localStorage.getItem("user_role"));
-  console.log("roles", roles);
 
-  var Routes = roles == "SUPERADMIN" ? superadmin_header : admin_header;
+
+  const routes = roles === "SUPERADMIN" ? superadmin_header : admin_header;
+
+  const getIconClass = (name) => {
+    switch (name.toLowerCase()) {
+      case 'dashboard':
+        return 'material-symbols-outlined';
+      case 'employees':
+      case 'users':
+      case 'position':
+        return 'fe fe-users';
+      case 'transaction':
+      case 'withdrawal':
+      case 'deposit':
+        return 'fe fe-dollar-sign';
+      case 'reports':
+        return 'fe fe-file-text';
+      case 'trade history':
+        return 'fe fe-activity';
+      case 'login status':
+        return 'fe fe-check-square';
+      default:
+        return '';
+    }
+  };
 
   return (
     <div className="dlabnav follow-info">
       <div className="menu-scroll">
         <div className="dlabnav-scroll mm-active">
           <ul className="metismenu mm-show" id="menu">
-            {Routes &&
-              Routes.map((data, index) => (
-                <li key={index} className="mm-active">
-                  <Link to={data.route} aria-expanded="false">
-                    <i className={data.Icon}></i>
-                    <span className="nav-text">{data.name}</span>
-                  </Link>
-                </li>
-              ))}
+            {routes && routes.map((data) => (
+              <li key={data.id} className="mm-active">
+                <Link to={data.route} aria-expanded="false">
+                  <i className={getIconClass(data.name)}></i>
+                  <span className="nav-text">{data.name}</span>
+                </Link>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
