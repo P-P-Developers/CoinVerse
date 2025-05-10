4d7227bf834407d84996a31e14fecb17f29137a5
snehpnp <snehpnp@gmail.com>
2025-05-10 16:59:16 +0530

    fix: Update user selection logic to include transactionwise field and adjust option determination

--- Backend/App/Controllers/Users/Users.controller.js
+++ Backend/App/Controllers/Users/Users.controller.js
@@ -158,8 +158,10 @@ class Users {
     try {
       const { userid } = req.body;
       const result1 = await User_model.find({ _id: userid })
-        .select("parent_id pertrade perlot")
+        .select("parent_id pertrade perlot transactionwise")
         .sort({ createdAt: -1 });
+
+        console.log("result1", result1);
 
       // Access the first element of result1 to avoid errors
       const user = result1[0];
@@ -173,9 +175,9 @@ class Users {
       }
 
       let Obj = {
-        option: user.pertrade && user.pertrade !== 0 ? "pertrade" : "perlot",
+        option: user.pertrade && user.pertrade !== 0 ? "pertrade" : user.transactionwise !== 0 ?"transactionwise" :"perlot",
         value1:
-          user.pertrade && user.pertrade !== 0 ? user.pertrade : user.perlot,
+          user.pertrade && user.pertrade !== 0 ? user.pertrade :user.transactionwise !== 0 ?user.transactionwise : user.perlot,
         crypto: result.crypto || 100,
         forex: result.forex || 100,
       };
