be5d9ca1a041757473a0f64c73558c349925c959
snehpnp <snehpnp@gmail.com>
2025-05-19 14:39:41 +0530

    OK

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 // Find Duplicate Elements in an Array
 
-let a = [ 1,2,3,1,1]
-let Duplicate = []
+let a = [5,8,2,9,1,4,3];
+let Duplicate = [];
 
 // for(let i=0;i<a.length;i++){
 //     for(let j=i+1;j<a.length;j++){
@@ -11,7 +11,19 @@ let Duplicate = []
 //     }
 // }
 
+// SORT WITH OUT METHOD
 
 
+let Sort = [];
 
-console.log(Duplicate)
+for(let i=0;i<a.length;i++){
+    for(let j=i+1;j<a.length;j++){
+        if(a[i]>a[j]){
+            let temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+        }
+    }
+}
+
+console.log(a);
