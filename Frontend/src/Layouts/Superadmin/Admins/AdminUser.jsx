addce12b3e74703be1c98c5d23e28ae9e69bb01c
Aaditya-Gurjar <aaditya2002.com@gmail.com>
2025-05-20 10:55:27 +0530

    refactor: Improve code formatting and enhance date rendering in AdminUser component

--- Frontend/src/Layouts/Superadmin/Admins/AdminUser.jsx
+++ Frontend/src/Layouts/Superadmin/Admins/AdminUser.jsx
@@ -26,55 +26,58 @@ const AdminUser = () => {
 
     { Header: "Balance", accessor: "Balance" },
     {
-        Header: "ActiveStatus",
-        accessor: "ActiveStatus",
-        Cell: ({ cell }) => (
-          <span
-            style={{
-              height: '15px',
-              width: '15px',
-              backgroundColor: cell.value == 1 ? 'green' : 'red',
-              borderRadius: '50%',
-              display: 'inline-block',
-            }}
-          ></span>
-        ),
-      }
-      ,
-      { Header: "PhoneNo", accessor: "PhoneNo" },
-      { Header: "Licence", accessor: "Licence" },
-    { Header: "createdAt", accessor: "createdAt" ,
-        Cell: ({ cell }) => {
-            return fDateTime(cell.value)
-   
-           },
+      Header: "ActiveStatus",
+      accessor: "ActiveStatus",
+      Cell: ({ cell }) => (
+        <span
+          style={{
+            height: '15px',
+            width: '15px',
+            backgroundColor: cell.value == 1 ? 'green' : 'red',
+            borderRadius: '50%',
+            display: 'inline-block',
+          }}
+        ></span>
+      ),
+    }
+    ,
+    { Header: "PhoneNo", accessor: "PhoneNo" },
+    { Header: "Licence", accessor: "Licence" },
+    {
+      Header: "createdAt", accessor: "createdAt",
+      Cell: ({ cell }) => {
+        return fDateTime(cell.value)
+
+      },
     },
-    { Header: "Start_Date", accessor: "Start_Date",
-        Cell: ({ cell }) => {
-            return fDateTime(cell.value)
-   
-           },
-     },
-    { Header: "End_Date", accessor: "End_Date",
-        Cell: ({ cell }) => {
-            return fDateTime(cell.value)
-   
-           },
+    {
+      Header: "Start_Date", accessor: "Start_Date",
+      Cell: ({ cell }) => {
+        return fDateTime(cell.value)
+
+      },
     },
-   
+    {
+      Header: "End_Date", accessor: "End_Date",
+      Cell: ({ cell }) => {
+        return fDateTime(cell.value)
+
+      },
+    },
+
   ];
 
-//   Function to get user history
+  //   Function to get user history
   const getuserallhistory = async () => {
     try {
       const data = { userid: id };
       const response = await getadminuserdetail(data);
-      const filteruser = response.data && response.data.filter((item)=>{
-        return item.Role ==="USER"
+      const filteruser = response.data && response.data.filter((item) => {
+        return item.Role === "USER"
       })
       setData(filteruser);
     } catch (error) {
-     return error
+      return error
     }
   };
 
@@ -83,9 +86,9 @@ const AdminUser = () => {
   }, [id]);
 
 
-  
 
-  
+
+
   return (
     <>
       <div>
@@ -98,15 +101,15 @@ const AdminUser = () => {
                     <h4 className="card-title">Admin User</h4>
                   </div>
                   <Link
-                  to="/superadmin/admin"
-                  className="float-end mb-4 btn btn-primary"
-                >
-                  Back
-                </Link>
+                    to="/superadmin/admin"
+                    className="float-end mb-4 btn btn-primary"
+                  >
+                    Back
+                  </Link>
                 </div>
                 <div className="card-body p-0">
                   <div className="tab-content" id="myTabContent1">
-                 
+
                     <div
                       className="tab-pane fade show active"
                       id="Week"
