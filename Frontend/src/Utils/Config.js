c82965a4576e4d2d2a93f2eb050d44941c9cb6a5
ajay90989 <ajay.office.patidar@gmail.com>
2025-08-05 18:37:16 +0530

    done

--- Frontend/src/Utils/Config.js
+++ Frontend/src/Utils/Config.js
@@ -1,11 +1,11 @@
 const port = localStorage.getItem("Port");
 
-export const base_url = `http://localhost:8800/`;
+// export const base_url = `http://localhost:8800/`;
 // export const base_url = `http://185.209.75.198:8800/`;
 // export const base_url = `https://trade.tradestreet.in/backend/`;
 
 
-// export const base_url = `${window.location.origin}/backend/`;
+export const base_url = `${window.location.origin}/backend/`;
 export const socket_url = `https://trade.tradestreet.in:1003/`;
 export const socket_url_web = `https://185.209.75.198:1002/`;
 
